4d1de37dfacca1f752781d5259eb970b8fc88f7e
Biw_CS. <Piyawatbiwkai@gmail.com>
2025-08-20 08:05:55 +0700

    Add files via upload

--- DataStruct/DataStruct/src/oddAndEven.java
+++ DataStruct/DataStruct/src/oddAndEven.java
@@ -1,57 +1,72 @@
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class oddAndEven {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         try {
             Random random = new Random();
-            int n = random.nextInt(11) + 10;    //random number
-            System.out.println("--- Random Numbers [" + n + "] ---");   //size
-            Stack evenStack = new Stack();      //Stack เลขคู่
-            Stack Odd = new Stack();        //Satck เลขคี่
-            for (int i = 0; i < n; i++) {
-                int number = random.nextInt(101);   //สุ่มเลขตั้งแต่ 1 - 100
-                System.out.print(number + " ");     //ปริ้นเลขที่สุ่มได้
-                if(number % 2 == 0){
-                    evenStack.push(number);
+            Quese Odd = new Quese();
+            Quese Even = new Quese();
+            int number = random.nextInt(11) + 10;
+            System.out.println("--- Random Numbers[" + number + "] ---");
+            for (int i = 0; i < number; i++) {
+                int r = random.nextInt(101);
+                System.out.print(r + " ");
+                if(r % 2 == 0){
+                    Even.enQuese(r);    //เอาข้อมูลเข้าคิว
                 }
                 else{
-                    Odd.push(number);
+                    Odd.enQuese(r);
                 }
             }
             System.out.println();
-            System.out.println("--- Even Stack [" + evenStack.getSize() + "] ---");     //size Even
-            while (!evenStack.isEmptyStack()) {     //ปริ้นข้อมูลที่อยู่ใน Stack เลขคู่
-                System.out.print(evenStack.pop() + " ");    
+            System.out.println("---Even Quese[" + Even.getSize() + "]---");
+
+            int sizeEven = Even.getSize();
+            for (int i = 0; i < sizeEven; i++) {    //ปริ้นข้อมูลคิวออกมาทั้งหมด
+                Object d = Even.deQuese();      //ข้อมูลเก็บลงใน Object ตัวใหม่
+                System.out.print((int)d + " ");
+                Even.enQuese(d);    //เอาไปใส่ไว้ใน คิวต่อ  
             }
             System.out.println();
-            System.out.println("--- Odd Stack [" + Odd.getSize() + "] ---");        //size Odd
-            while (!Odd.isEmptyStack()) {       //ปริ้นข้อมุลที่อยู่ใน Stack เลขคี่
-                System.out.print(Odd.pop() + " ");
+            System.out.println("---Odd Quese[" + Odd.getSize() + "]---");
+            
+            int sizeOdd = Odd.getSize();
+            for (int i = 0; i < sizeOdd; i++) {     //ปริ้นข้อมูลคิวออกมาทั้งหมด
+                Object d = Odd.deQuese();       //ข้อมูลเก็บลงใน Object ตัวใหม่
+                System.out.print((int)d + " ");
+                Odd.enQuese(d);     //เอาไปใส่ไว้ใน คิวต่อ
+            }
+            System.out.println();
+            int countOdd = 0;
+            int countEven = 0;
+            while (!Even.isEmptyQuese() && !Odd.isEmptyQuese()) {
+                int frontEven = (int)Even.front();      //ตัวหน้าของ Even
+                int frontOdd = (int)Odd.front();        //ตัวหน้าของ Odd
+                if(frontEven > frontOdd){       //ถ้า Even ชนะ
+                    Even.deQuese();     //ลบข้อมูลตัวหน้าออกจาก คิว
+                    Odd.enQuese(Odd.front());   //เอาข้อมูลของ Odd ตัวหน้าเข้าไปอยู่ในคิวใหม่
+                    Odd.deQuese();  //ลบข้อมูลของ Odd ออกไป
+                    countEven++;    
+                }
+                else if(frontOdd > frontEven){      //ถ้า Odd win
+                    Odd.deQuese();      //ลบข้อมูลตัวหน้าของ Odd ออกไป
+                    Even.enQuese(Even.front());     //เอาข้อมูลตัวหน้าของ Even เข้าไปอยู่ในคิว
+                    Even.deQuese(); //ลบข้อมูลของ Even ออกไป
+                    countOdd++;
+                }
+            }
+            if(countEven > countOdd){
+                System.out.println("===> Winner is Even!");
+                System.out.println("Round = " + (countEven + countOdd) + ", Win=(" + "E:" + countEven + ",O:" + countOdd + "), Odd Left = " + Odd.getSize());
+            }
+            else if(countOdd > countEven){
+                System.out.println("===> Winner is Odd!");
+                System.out.println("Round = " + (countOdd + countEven) + ", Win=(E:" + countEven + ",O:" + countOdd + "), Even Left = " + Even.getSize());
             }
 
-            while (!evenStack.isEmptyStack() && !Odd.isEmptyStack()) {      //เช็คค่าบนสุดใน Stack 
-                int evenTop = (Integer)evenStack.peek();
-                int OddTop = (Integer)Odd.peek();
-                if(evenTop > OddTop){
-                   evenStack.pop();
-                }
-                else if(OddTop > evenTop){
-                    Odd.pop();
-                }
-            }
-            System.out.println();
-            if(evenStack.isEmptyStack()){
-                System.out.println("===> Even is the Winner!");
-            }
-            else if(Odd.isEmptyStack()){
-                System.out.println("===> Odd is the Winner!");
-            }
-            
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        sc.close();
-    }
+    }  
 }
