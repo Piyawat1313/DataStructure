33df57996e9aafd871723fa26deef79eee4f34b9
Biw_CS. <Piyawatbiwkai@gmail.com>
2025-08-21 07:53:51 +0700

    Add files via upload

---
+++ DataStruct/DataStruct/src/Quese.java
@@ -0,0 +1,63 @@
+public class Quese {
+    private int maxQueseSize = 100;
+    private int queseFront = 0;
+    private int queseRear = maxQueseSize - 1;
+    private int count = 0;
+    private Object[]list;
+    public Quese(){
+        list = new Object[maxQueseSize];
+    }
+    public Quese(int size){
+        list = new Object[size];
+        maxQueseSize = size;
+        queseRear = maxQueseSize - 1;
+    }
+    public void innitializeQuese(){
+        for (int i = queseFront; i < queseRear; i = (i + 1) % maxQueseSize) {
+            list[i] = null;
+            queseFront = 0;
+            queseRear = maxQueseSize - 1;
+            count = 0;
+        }
+    }
+    public boolean isEmptyQuese(){
+        return count == 0;
+    }
+    public boolean isFullQuese(){
+        return count == maxQueseSize;
+    }
+    public void enQuese(Object newItem)throws Exception{
+        if(isFullQuese()){
+            throw new Exception("QueseUnderFlow");
+        }
+        queseRear = (queseRear + 1) % maxQueseSize;
+        count++;
+        list[queseRear] = newItem;
+    }
+    public void enQuese()throws Exception{
+        if(isFullQuese()){
+            throw new Exception("QueseUnderFlow");
+        }
+        count--;
+        list[queseFront] = null;
+        queseFront = (queseFront + 1) % maxQueseSize;
+    }
+    public Object front()throws Exception{
+        if(isEmptyQuese()){
+            throw new Exception("QueseUnderFlow");
+        }
+        return list[queseFront];
+    }
+    public Object back()throws Exception{
+        if(isEmptyQuese()){
+            throw new Exception("QueseUnderFlow");
+        }
+        return list[queseRear];
+    }
+    public int getSize(){
+        return count;
+    }
+    public int getMaxSize(){
+        return maxQueseSize;
+    }
+}
