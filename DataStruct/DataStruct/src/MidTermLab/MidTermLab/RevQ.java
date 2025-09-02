24588faccf7f53d5c5efd957ede3ceeff9ca579e
Biw_CS. <Piyawatbiwkai@gmail.com>
2025-09-02 22:07:25 +0700

    Update RevQ.java

--- DataStruct/DataStruct/src/MidTermLab/MidTermLab/RevQ.java
+++ DataStruct/DataStruct/src/MidTermLab/MidTermLab/RevQ.java
@@ -1,3 +1,4 @@
+package Midterm.MidTermLab;
 /*
 NO: xx
 ID: xxxxxxxxx
@@ -14,26 +15,71 @@ public class RevQ {
     public static void inputQ(Queue q) {
         Scanner keyboard = new Scanner(System.in);
         int data;
-        try{
+        try {
             do {
                 data = keyboard.nextInt();
-                if(data>=0)
+                if (data >= 0)
                     q.enQueue(data);
-            } while(data>=0);
-        } catch(Exception e) {}
-    } 
+            } while (data >= 0);
+        } catch (Exception e) {
+        }
+    }
 
     public static void showQ(Queue q) {
-		// TO DO
+        // TO DO
+        try {
+            while (!q.isEmptyQueue()) {
+                System.out.print(q.front() + " | ");
+                q.deQueue();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void reverseOddQ(Queue q) {
-		// TO DO
-        if(data )
+        // TO DO
+        
+        //แยกข้อมูล
+        Stack oddStack = new Stack();
+        Queue even = new Queue();
+        try {
+            while (!q.isEmptyQueue()) {
+                int number = (int) q.front();
+                q.deQueue();
+                if (number % 2 != 0) {
+                    oddStack.push(number);
+                    even.enQueue("Odd");   //จำตำแหน่งเดิมของเลขคี่
+                } else {
+                    even.enQueue(number);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        //ใส่เลขคี่ที่สลับในStack
+        try {
+            while (!even.isEmptyQueue()) {
+                Object item = even.front();
+                even.deQueue();
+                if (item.equals("Odd")) {
+                    int score = (int) oddStack.pop();   //ใส่คืนตำแหน่งเดิม
+                    q.enQueue(score);
+                } else {
+                    q.enQueue(item);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public static void main(String[] args) {
-        Queue q=new Queue();
+        Queue q = new Queue();
         inputQ(q);
         reverseOddQ(q);
         showQ(q);
